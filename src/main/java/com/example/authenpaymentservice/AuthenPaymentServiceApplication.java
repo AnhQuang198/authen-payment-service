6faac48990dafb353c532510a7336467a0f60e74
Quang <quang@Quangs-MacBook-Pro.local>
2022-02-09 21:01:13 +0700

    init project

---
+++ src/main/java/com/example/authenpaymentservice/AuthenPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.authenpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenPaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/authenpaymentservice/AuthenPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authenpaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
