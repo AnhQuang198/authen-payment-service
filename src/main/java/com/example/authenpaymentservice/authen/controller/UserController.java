5e85e54418b045f3a9a4691f009773cb793255bf
Quang <quang@Quangs-MacBook-Pro.local>
2022-02-23 21:01:20 +0700

    update

--- src/main/java/com/example/authenpaymentservice/authen/controller/UserController.java
+++ src/main/java/com/example/authenpaymentservice/authen/controller/UserController.java
@@ -1,16 +1,23 @@
 package com.example.authenpaymentservice.authen.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/user")
+@CrossOrigin(value = "*", maxAge = 3600L)
 public class UserController {
 
-    @GetMapping
+    @GetMapping("/me")
     public ResponseEntity<?> getProfile() {
+        return null;
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<?> get() {
         return null;
     }
 }
